ab2792dc8caa8176699a9468818c33301f407ed6
PanDav1d <muellerdavid.3008@gmail.com>
2026-02-01 00:37:13 +0100

    solve: exercise 1.14*

--- part1/anecdotes/src/App.jsx
+++ part1/anecdotes/src/App.jsx
@@ -19,16 +19,23 @@ const App = () => {
     const copyVotes = [...votes];
     copyVotes[selected] += 1;
     setVotes(copyVotes);
+    setMostVoted(anecdotes[copyVotes.indexOf(Math.max(...copyVotes))]);
   };
+  const [mostVoted, setMostVoted] = useState(anecdotes[0]);
 
   return (
     <div>
+      <h1>Anecdote of the day</h1>
       <p>{anecdotes[selected]}</p>
       <p>has {votes[selected]} votes</p>
       <button onClick={() => vote()}>vote</button>
       <button onClick={() => setSelected(getRandomInt(anecdotes.length))}>
         next anecdote
       </button>
+
+      <h1>Anecdote with most votes</h1>
+      <p>{mostVoted}</p>
+      <p>has {Math.max(...votes)} votes</p>
     </div>
   );
 };
