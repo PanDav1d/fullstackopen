77da50e267bea44f43b8c0b8541e568cab1b79c2
PanDav1d <muellerdavid.3008@gmail.com>
2026-02-01 14:59:07 +0100

    solve: exercise 2.12

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -41,6 +41,10 @@ const App = () => {
       : setPersons(persons.concat(newPerson));
     setNewName("");
     setNewNumber("");
+
+    axios
+      .post(`http://localhost:3001/persons`, newPerson)
+      .then((response) => console.log(response));
   };
 
   return (
