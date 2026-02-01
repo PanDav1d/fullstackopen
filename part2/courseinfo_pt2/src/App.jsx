8d58fcfdad3ec0d91b81ca6eaf1b4fccb5f11cc1
PanDav1d <muellerdavid.3008@gmail.com>
2026-02-01 12:17:32 +0100

    solve: exercise 2.3*

--- part2/courseinfo_pt2/src/App.jsx
+++ part2/courseinfo_pt2/src/App.jsx
@@ -15,10 +15,12 @@ const Content = ({ parts }) => (
 const Header = ({ name }) => <h1>{name}</h1>;
 
 const Course = ({ course }) => {
+  const total = course.parts.reduce((akku, num) => akku + num.exercises, 0);
   return (
     <>
       <Header name={course.name} />
       <Content parts={course.parts} />
+      <p>total of {total} exercises</p>
     </>
   );
 };
